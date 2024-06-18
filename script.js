a7a36d6d97d919a605415f4336c7e273b54df15a
SembodoGelar <sembodogelar6@gamail.com>
2024-06-18 21:07:45 +0700

    add home.Frame variable

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 document.getElementById('greetButton').addEventListener('click', function() {
     var name = document.getElementById('nameInput').value;
     var greetingMessage = document.getElementById('greetingMessage');
+    var homeFrame = document.getElementById('homeFrame')
 
     if (name) {
         greetingMessage.textContent = 'Hello, ' + name + '!';
+        homeFrame.style.display = 'block';
     } else {
         greetingMessage.textContent = 'Please enter your name.';
+        homeFrame.style.display = 'none';
     }
 });
